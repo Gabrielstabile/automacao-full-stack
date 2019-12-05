d06a4e2f14785e0ca773b63cad38928bc775a953
gabrielstabilee <gabriel.feliciano.stabile@everis.com>
2019-12-05 16:58:56 -0300

    Mouse hover implementation

--- Ruby/capy/spec/hover_spec.rb
+++ Ruby/capy/spec/hover_spec.rb
@@ -5,9 +5,25 @@ describe 'Mouse Hover', :hovers do
     end
 
     it 'quando passo o mouse sobre o blade'do
+        cardBlade = find('img[alt*=Blade]')
+        cardBlade.hover
 
-        
-    
+        expect(page).to have_content("Nome: Blade")
+    end
+
+    it 'quando passo o mouse sobre o pantera negra'do 
+        cardPanteraNegra = find('img[alt^=Pantera]')
+        cardPanteraNegra.hover
+
+        expect(page).to have_content("Nome: Pantera Negra")
+    end
+
+
+    it 'quando passo o mouse sobre o Homem Aranha'do 
+        cardPanteraNegra = find('img[alt$=Aranha]')
+        cardPanteraNegra.hover
+
+        expect(page).to have_content("Nome: Homem Aranha")
     end
 
 end
