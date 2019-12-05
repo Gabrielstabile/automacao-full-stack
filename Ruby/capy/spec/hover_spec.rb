2848247d87b5f22dab05f096a031ecc9f28f1e8b
gabrielstabilee <gabriel.feliciano.stabile@everis.com>
2019-12-05 16:10:36 -0300

    first structure for mouse hover

---
+++ Ruby/capy/spec/hover_spec.rb
@@ -0,0 +1,13 @@
+describe 'Mouse Hover', :hovers do
+
+    before(:each)do
+        visit 'http://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'quando passo o mouse sobre o blade'do
+
+        
+    
+    end
+
+end
